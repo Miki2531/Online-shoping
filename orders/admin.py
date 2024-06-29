b94042ece0de2a7ac9f6c42b6e21fda458cbfeb6
Mikiyas shemelis <mikiyasshemelis46gmail.com>
2024-06-29 18:32:37 +0300

    Add referencing a stripe id payment in orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,16 @@
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 from .models import Order, OrderItem
 
+def order_paymet(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+order_paymet.short_description = 'Stripe payment'
+    
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -8,7 +18,7 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 
-                    'email', 'address', 'postal_code', 'city', 'paid',
+                    'email', 'address', 'postal_code', 'city', 'paid', 'order_payment',
                     'created_at', 'updated_at']
     list_filter = ['paid', 'created_at', 'updated_at']
     inlines = [OrderItemInline]
